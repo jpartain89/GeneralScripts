d9e3e835c4674cf89af387e83759a1fde47af420
jpartain89 <jpartain89@gmail.com>
2016-08-07 22:46:19 -0500

    UPdates

--- RapidLoopAgent.sh
+++ RapidLoopAgent.sh
@@ -2,7 +2,24 @@
 
 # RapidLoop Agent Install - Installs Repo, updates, and runs the installer
 
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+. $DIR/myfunctions/allunix.sh
 
+yes_sudo
 
-echo "deb http://packages.rapidloop.com/debian stable main" | sudo tee /etc/apt/sources.list.d/rapidloop.list
+echo "deb http://packages.rapidloop.com/debian stable main" | tee /etc/apt/sources.list.d/rapidloop.list
+
 apt-key adv --keyserver pgp.mit.edu --recv-keys 1E1BACF5
+
+apt-get update && apt-get install opsdash-agent
+
+echo ""
+echo "The agent should have installed correctly."
+echo "Now, after pressing enter, nano will open to the agent config file"
+echo "For you to look over and setup."
+press_enter
+nano /etc/opsdash/agent.cfg
+echo ""
+echo "Now starting the agent service."
+press_enter
+service opsdash-agentd start
