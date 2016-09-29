ea949264712849943e9f0af9f66f2bcacf4bbaaa
jpartain89 <Jpartain89@gmail.com>
2016-09-29 09:16:30 -0500

    Updates

--- bash/server_ping.sh
+++ bash/server_ping.sh
@@ -2,7 +2,7 @@
 
 # Script to run a ping test and subsequently shoot a text.
 
-if ping -c 1000 8.8.8.8 > /dev/null; then \
+if ping -c 10 8.8.8.8 > /dev/null; then \
     curl -X POST http://textbelt.com/text \
     -d number=2149239252 \
     -d "message=Mark's Server is Down.... This is to annoy you!"
