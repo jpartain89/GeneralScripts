444a7dc338c5bd162d7640599dcc620364330733
jpartain89 <Jpartain89@gmail.com>
2017-01-05 13:18:30 -0600

    Removed the "MyFunctions" submodule, as that has its own installer script now.
    Updating my ans-apt wrapper script to split my various machines apart
    more.
    
    Removed the "server_ping.sh" and "domotz_deb" files, as I'm no longer
    using domotz, and the ping script was more or less pointless.

--- bash/server_ping.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Script to run a ping test and subsequently shoot a text.
-
-if ping -c 10 8.8.8.8 > /dev/null; then \
-    curl -X POST http://textbelt.com/text \
-    -d number=2149239252 \
-    -d "message=Mark's Server is Down.... This is to annoy you!"
-fi
