6b635fabd7831979a452680edd094e8caced53f3
jpartain89 <jpartain89@gmail.com>
2017-03-18 00:19:39 -0500

    Changes to be committed: 	modified:   automap-crontab.sh 	Minor update for loggin

--- bash/automap-crontab.sh
+++ bash/automap-crontab.sh
@@ -5,9 +5,10 @@
 export PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin
 
 LOGDIR="/var/log/git-automap"
-LOGFILE="${LOGDIR}/$(date '+%Y-%m-%d_%H-%M-%S').log"
+LOGFILE="${LOGDIR}/automap_$(date '+%Y-%m-%d_%H-%M-%S').log"
 
 [ -d "$LOGDIR" ] || sudo mkdir -p "$LOGDIR"
-[ -f "$LOGFILE" ] || sudo touch "$LOGFILE"j
+[ -f "$LOGFILE" ] || sudo touch "$LOGFILE"
+find "$LOGDIR/" -type f -mtime +7 -exec rm {} \;
 
 cd /home/jpartain89/git/dotfiles && /usr/local/bin/git-automap >>"$LOGFILE" 2>&1
