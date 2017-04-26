fafb0fedb7a5e401ad868bea5675e42da9081860
jpartain89 <jpartain89@gmail.com>
2017-04-26 08:11:13 -0500

    Removed `rsync-flags` for more verbose logging

--- bash/rsync_crontab.sh
+++ bash/rsync_crontab.sh
@@ -2,15 +2,11 @@
 
 # Bash Script to run `flock` and `rsync_tmbackup.sh` through crontab
 
-FLOCK="$(command -v flock)"
-LOCK_FILE="/tmp/rsync.lock"
-RSYNC_TMBACKUP="$(command -v rsync_tmbackup.sh)"
-RSYNC_BACKUP_OPTS="-D --compress --numeric-ids --links --hard-links --one-file-system --no-itemize-changes --times --recursive --perms --owner --group"
-SOURCE="/"
-DEST="jpartain89@192.168.1.15:/Volumes/8TB_EXT/Backups/rpi3"
-EXCLUDE_FILE="/home/jpartain89/exclude-file.txt"
 LOG_FILE="/var/log/rsync-backup.log"
+[  ! -e "${LOG_FILE}" ] && sudo touch "${LOG_FILE}"
+sudo chmod -R 0664 "${LOG_FILE}"
 
-( "$FLOCK" -n "$LOCK_FILE" sh -c \
-    "sudo bash ${RSYNC_TMBACKUP} --rsync-set-flags ${RSYNC_BACKUP_OPTS} \
-    ${SOURCE} ${DEST} ${EXCLUDE_FILE} 1>${LOG_FILE} 2>&1" & ) &
+( "$(command -v flock)" -n /tmp/rsync.lock sh -c \
+    "sudo bash $(command -v rsync_tmbackup.sh) \
+    / jpartain89@192.168.1.15:/Volumes/8TB_EXT/Backups/rpi3 \
+    /home/jpartain89/exclude-file.txt 1>> ${LOG_FILE} 2>&1" & ) &
