c893ee185c282caea2e0ce583823c1b970179b2b
jpartain89 <jpartain89@gmail.com>
2017-04-28 09:00:51 -0500

    Changes to be committed: 	modified:   bash/rsync_crontab.sh

--- bash/rsync_crontab.sh
+++ bash/rsync_crontab.sh
@@ -6,6 +6,8 @@ LOG_FILE="/var/log/rsync-backup.log"
 [  ! -e "${LOG_FILE}" ] && sudo touch "${LOG_FILE}"
 sudo chmod -R 0664 "${LOG_FILE}"
 
+trap 'sudo rm /tmp/rsync.lock; exit 1' SIGHUP SIGINT SIGTERM
+
 ( "$(command -v flock)" -n /tmp/rsync.lock sh -c \
     "sudo bash $(command -v rsync_tmbackup.sh) \
     / jpartain89@192.168.1.15:/Volumes/8TB_EXT/Backups/rpi3 \
