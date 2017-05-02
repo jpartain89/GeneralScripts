390572d9e7c8b16ef9abcb6b20f65f2bda2a9f14
jpartain89 <jpartain89@gmail.com>
2017-05-02 09:26:04 -0500

    Changes to be committed: 	new file:   mp4_automator_script.sh

---
+++ bash/mp4_automator_script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+# Trying to bash-automate `sickbeard_mp4_automator`
+
+MANUAL_FILE="/Users/jpartain89/git/sickbeard_mp4_automator/manual.py"
+
+trap "echo 'exiting', exit 1" SIGQUIT SIGTERM SIGINT
+
+echo -n "Please, type the FULL path to the directory you want to work on."
+read -r IN_DIRECTORY
+
+while IFS= read -r IN_FILES; do
+    "${MANUAL_FILE}" -a -nm -i "${IN_FILES}"
+done < <(find "${IN_DIRECTORY}" -iname "*.mkv" -exec echo {} \;)
