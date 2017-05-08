c6c4108882485a6181431554e2a480c752f64e37
jpartain89 <jpartain89@gmail.com>
2017-05-08 16:31:04 -0500

    Changes to be committed: 	modified:   bash/rsync_crontab.sh

--- bash/rsync_crontab.sh
+++ bash/rsync_crontab.sh
@@ -2,13 +2,11 @@
 
 # Bash Script to run `flock` and `rsync_tmbackup.sh` through crontab
 
-LOG_FILE="/var/log/rsync-backup.log"
-[  ! -e "${LOG_FILE}" ] && sudo touch "${LOG_FILE}"
-sudo chmod -R 0664 "${LOG_FILE}"
+exec 1> >(logger -t $(basename $0)) 2>&1
 
-trap 'sudo rm /tmp/rsync.lock; exit 1' SIGHUP SIGINT SIGTERM
+trap 'sudo rm /tmp/rsync.lock; exit 1' SIGHUP SIGINT SIGTERM SIGKILL
 
-( "$(command -v flock)" -n /tmp/rsync.lock sh -c \
-    "sudo bash $(command -v rsync_tmbackup.sh) \
-    / jpartain89@192.168.1.15:/Volumes/8TB_EXT/Backups/rpi3 \
-    /home/jpartain89/exclude-file.txt 1>> ${LOG_FILE} 2>&1" & ) &
+( "$(command -v flock)" -n /tmp/rsync.lock \
+  $(sudo bash $(command -v rsync_tmbackup.sh) \
+  / jpartain89@192.168.1.15:/Volumes/8TB_EXT/Backups/rpi3 \
+  /home/jpartain89/exclude-file.txt & ) & )
