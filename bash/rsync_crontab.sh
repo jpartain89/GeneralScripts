7e4e918d4bdc6e90d64e5659063ba583d68e4851
jpartain89 <jpartain89@gmail.com>
2017-05-19 14:04:18 -0500

    Changes to be committed: 	modified:   bash/rsync_crontab.sh

--- bash/rsync_crontab.sh
+++ bash/rsync_crontab.sh
@@ -6,7 +6,7 @@ exec 1> >(logger -t $(basename $0)) 2>&1
 
 trap 'sudo rm /tmp/rsync.lock; exit 1' SIGHUP SIGINT SIGTERM SIGKILL
 
-( "$(command -v flock)" -n /tmp/rsync.lock \
+( "$(command -v flock)" -n 8 || exit 1
   $(sudo bash $(command -v rsync_tmbackup.sh) \
   / jpartain89@192.168.1.15:/Volumes/8TB_EXT/Backups/rpi3 \
-  /home/jpartain89/exclude-file.txt & ) & )
+  /home/jpartain89/exclude-file.txt & ) & ) 8> /tmp/rsync.lock
