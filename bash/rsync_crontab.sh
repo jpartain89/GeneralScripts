da5c1945967ebda63400e97ad0a4e9cbd5378d30
jpartain89 <jpartain89@gmail.com>
2017-06-02 22:14:31 -0500

    Updates

--- bash/rsync_crontab.sh
+++ bash/rsync_crontab.sh
@@ -7,12 +7,12 @@ exec 1> >(logger -t "$(basename "$0")") 2>&1
 logfile="/var/log/$(basename "$0").log"
 [ -e "$logfile" ] || sudo touch "$logfile"
 
-trap 'sudo rm /tmp/rsync.lock; exit 1' SIGHUP SIGINT SIGTERM EXIT
+trap 'sudo rm -r /tmp/rsync.lock; exit 1' SIGHUP SIGINT SIGTERM EXIT
 
 if mkdir /tmp/rsync.lock 2>/dev/null; then
     ( sudo bash "$(command -v rsync_tmbackup.sh)" \
         / jpartain89@192.168.1.15:/Volumes/8TB_EXT/Backups/rpi3 \
-        /home/jpartain89/exclude-file.txt & ) & 
+        /home/jpartain89/exclude-file.txt & ) &
 else
         echo "Sorry, the lockfile currently exists. Exiting."
         exit 1
