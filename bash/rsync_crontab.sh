917d43826fa769426506f963a3fb0e61792b68ac
jpartain89 <jpartain89@gmail.com>
2017-06-02 21:59:51 -0500

    Changed logging, added `EXIT` to trap

--- bash/rsync_crontab.sh
+++ bash/rsync_crontab.sh
@@ -5,13 +5,11 @@
 logfile="/var/log/$(basename "$0").log"
 [ -e "$logfile" ] || sudo touch "$logfile"
 
-exec 1> >(logger -t "$(basename "$0")" -f "$logfile") 2>&1
-
-trap 'sudo rm /tmp/rsync.lock; exit 1' SIGHUP SIGINT SIGTERM
+trap 'sudo rm /tmp/rsync.lock; exit 1' SIGHUP SIGINT SIGTERM EXIT
 
 (
     "$(command -v flock)" -n 8 || exit 1
-        sudo bash "$(command -v rsync_tmbackup.sh)" \
+        ( sudo bash "$(command -v rsync_tmbackup.sh)" \
         / jpartain89@192.168.1.15:/Volumes/8TB_EXT/Backups/rpi3 \
-        /home/jpartain89/exclude-file.txt &
+        /home/jpartain89/exclude-file.txt & ) &> "$logfile" 2>&1
 ) 8> /tmp/rsync.lock
