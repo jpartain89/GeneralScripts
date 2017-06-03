cb1060ded88008708855b2f15e67dfc67286621c
jpartain89 <jpartain89@gmail.com>
2017-06-02 22:12:24 -0500

    Trying different lockfile

--- bash/rsync_crontab.sh
+++ bash/rsync_crontab.sh
@@ -2,13 +2,18 @@
 
 # Bash Script to run `flock` and `rsync_tmbackup.sh` through crontab
 
-[ "${FLOCKER}" != "$0" ] && exec env FLOCKER="$0" flock -n "$0" "$0" "$@" || :
+exec 1> >(logger -t "$(basename "$0")") 2>&1
 
 logfile="/var/log/$(basename "$0").log"
 [ -e "$logfile" ] || sudo touch "$logfile"
 
 trap 'sudo rm /tmp/rsync.lock; exit 1' SIGHUP SIGINT SIGTERM EXIT
 
-sudo bash "$(command -v rsync_tmbackup.sh)" \
-    / jpartain89@192.168.1.15:/Volumes/8TB_EXT/Backups/rpi3 \
-    /home/jpartain89/exclude-file.txt &> "$logfile" 2>&1
+if mkdir /tmp/rsync.lock 2>/dev/null; then
+    sudo bash "$(command -v rsync_tmbackup.sh)" \
+        / jpartain89@192.168.1.15:/Volumes/8TB_EXT/Backups/rpi3 \
+        /home/jpartain89/exclude-file.txt
+else
+        echo "Sorry, the lockfile currently exists. Exiting."
+        exit 1
+fi
