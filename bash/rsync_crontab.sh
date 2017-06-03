9687499430956fbe885ebf89cb126cd37315dd95
jpartain89 <jpartain89@gmail.com>
2017-06-02 21:41:52 -0500

    Attempted `flock` updates

--- bash/rsync_crontab.sh
+++ bash/rsync_crontab.sh
@@ -2,11 +2,13 @@
 
 # Bash Script to run `flock` and `rsync_tmbackup.sh` through crontab
 
-exec 1> >(logger -t $(basename $0)) 2>&1
+exec 1> >(logger -t "$(basename "$0")") 2>&1
 
-trap 'sudo rm /tmp/rsync.lock; exit 1' SIGHUP SIGINT SIGTERM SIGKILL
+trap 'sudo rm /tmp/rsync.lock; exit 1' SIGHUP SIGINT SIGTERM
 
-( "$(command -v flock)" -n 8 || exit 1
-  $(sudo bash $(command -v rsync_tmbackup.sh) \
-  / jpartain89@192.168.1.15:/Volumes/8TB_EXT/Backups/rpi3 \
-  /home/jpartain89/exclude-file.txt & ) & ) 8> /tmp/rsync.lock
+(
+    "$(command -v flock)" -n 8 || exit 1
+        sudo bash "$(command -v rsync_tmbackup.sh)" \
+        / jpartain89@192.168.1.15:/Volumes/8TB_EXT/Backups/rpi3 \
+        /home/jpartain89/exclude-file.txt &
+) 8> /tmp/rsync.lock
