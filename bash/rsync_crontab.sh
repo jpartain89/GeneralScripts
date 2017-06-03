25b36f782c77cf38994c0b02acb4dc95fa503be7
jpartain89 <jpartain89@gmail.com>
2017-06-02 22:02:58 -0500

    Trying different flock locking

--- bash/rsync_crontab.sh
+++ bash/rsync_crontab.sh
@@ -2,14 +2,13 @@
 
 # Bash Script to run `flock` and `rsync_tmbackup.sh` through crontab
 
+[ "${FLOCKER}" != "$0" ] && exec env FLOCKER="$0" flock -n "$0" "$0" "$@" || :
+
 logfile="/var/log/$(basename "$0").log"
 [ -e "$logfile" ] || sudo touch "$logfile"
 
 trap 'sudo rm /tmp/rsync.lock; exit 1' SIGHUP SIGINT SIGTERM EXIT
 
-(
-    "$(command -v flock)" -n 8 || exit 1
-        ( sudo bash "$(command -v rsync_tmbackup.sh)" \
-        / jpartain89@192.168.1.15:/Volumes/8TB_EXT/Backups/rpi3 \
-        /home/jpartain89/exclude-file.txt & ) &> "$logfile" 2>&1
-) 8> /tmp/rsync.lock
+sudo bash "$(command -v rsync_tmbackup.sh)" \
+    / jpartain89@192.168.1.15:/Volumes/8TB_EXT/Backups/rpi3 \
+    /home/jpartain89/exclude-file.txt &> "$logfile" 2>&1
