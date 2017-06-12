3e1ef8d7315bb7d8af6b46bf0346a629e3122211
jpartain89 <jpartain89@gmail.com>
2017-06-12 03:09:42 -0500

    Created Variables for the backup location info.

--- bash/rsync_crontab.sh
+++ bash/rsync_crontab.sh
@@ -4,6 +4,10 @@
 
 exec 1> >(logger -t "$(basename "$0")") 2>&1
 
+REMOTE_ADDRESS="jpartain89@192.168.1.15"
+REMOTE_DIR="/Volumes/2TB_RAID/Backups/rpi3"
+BACK_LOC="${REMOTE_ADDRESS}:${REMOTE_DIR}"
+
 logfile="/var/log/$(basename "$0").log"
 [ -e "$logfile" ] || sudo touch "$logfile"
 
@@ -11,8 +15,7 @@ trap 'sudo rm -r /tmp/rsync.lock; exit 1' SIGHUP SIGINT SIGTERM EXIT
 
 if mkdir /tmp/rsync.lock 2>/dev/null; then
     ( sudo bash "$(command -v rsync_tmbackup.sh)" \
-        / jpartain89@192.168.1.15:/Volumes/8TB_EXT/Backups/rpi3 \
-        /home/jpartain89/exclude-file.txt & ) &
+        / "$BACK_LOC" /home/jpartain89/exclude-file.txt & ) &
 else
         echo "Sorry, the lockfile currently exists. Exiting."
         exit 1
