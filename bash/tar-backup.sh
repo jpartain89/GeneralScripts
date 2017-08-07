6f1e095e51907b96678cf5de554e0445be8a76ff
jpartain89 <jpartain89@gmail.com>
2017-08-07 06:54:22 -0500

    Changes to be committed: 	modified:   bash/hbCLI 	new file:   bash/pip_freeze_update 	new file:   bash/tar-backup.sh

---
+++ bash/tar-backup.sh
@@ -0,0 +1,16 @@
+tar -cvpz \
+    --exclude=/proc \
+    --exclude=/tmp \
+    --exclude=/mnt \
+    --exclude=/dev \
+    --exclude=/sys \
+    --exclude=/lost+found \
+    --exclude=/run \
+    --exclude=/media \
+    --exclude=/var/log \
+    --exclude=/var/cache/apt/archives \
+    --exclude=/usr/src/linux-headers* \
+    --exclude=/home/*/.gvfs \
+    --exclude=/home/*/.cache \
+    --exclude=/home/*/.local \
+    --exclude=/home/*/.local/share/Trash / | nc -q 0 192.168.1.15 1024
