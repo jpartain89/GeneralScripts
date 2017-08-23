e99c159fec7d1e6ef3a5cefda1fa110861a52354
jpartain89 <jpartain89@gmail.com>
2017-08-22 20:17:48 -0500

    Changes to be committed: 	modified:   bash/rsync_crontab.sh

--- bash/rsync_crontab.sh
+++ bash/rsync_crontab.sh
@@ -1,22 +1,15 @@
 #!/bin/bash -e
 
-# Bash Script to run `flock` and `rsync_tmbackup.sh` through crontab
+# Bash Script to run `rsync_tmbackup.sh` through crontab
 
-exec 1> >(logger -t "$(basename "$0")") 2>&1
-
-REMOTE_ADDRESS="jpartain89@192.168.1.15"
-REMOTE_DIR="/Volumes/2TB_RAID/Backups/rpi3"
-BACK_LOC="${REMOTE_ADDRESS}:${REMOTE_DIR}"
+#REMOTE_ADDRESS="jpartain89@192.168.1.15"
+REMOTE_DIR="/media/BigPartition/Backups/rpi3"
+#BACK_LOC="${REMOTE_ADDRESS}:${REMOTE_DIR}"
 
 logfile="/var/log/$(basename "$0").log"
 [ -e "$logfile" ] || sudo touch "$logfile"
 
-trap 'sudo rm -r /tmp/rsync.lock; exit 1' SIGHUP SIGINT SIGTERM EXIT
+trap 'exit 1' SIGHUP SIGINT SIGTERM
 
-if mkdir /tmp/rsync.lock 2>/dev/null; then
-    ( sudo bash "$(command -v rsync_tmbackup.sh)" \
+( sudo bash "$(command -v rsync_tmbackup.sh)" \
         / "$BACK_LOC" /home/jpartain89/exclude-file.txt & ) &
-else
-        echo "Sorry, the lockfile currently exists. Exiting."
-        exit 1
-fi
