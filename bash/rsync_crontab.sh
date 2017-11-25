bb704bcf4645190d23d57647d7188e722089c5f9
jpartain89 <jpartain89@gmail.com>
2017-11-24 21:00:19 -0600

    Updates to start using again.

--- bash/rsync_crontab.sh
+++ bash/rsync_crontab.sh
@@ -2,14 +2,26 @@
 
 # Bash Script to run `rsync_tmbackup.sh` through crontab
 
-#REMOTE_ADDRESS="jpartain89@192.168.1.15"
-REMOTE_DIR="/media/BigPartition/Backups/rpi3"
-#BACK_LOC="${REMOTE_ADDRESS}:${REMOTE_DIR}"
+REMOTE_ADDRESS="jpartain89@192.168.1.15"
+REMOTE_DIR="/Volumes/8TB_EXT/Backups/rpi35/"
+BACK_LOC="${REMOTE_ADDRESS}:${REMOTE_DIR}"
 
 logfile="/var/log/$(basename "$0").log"
 [ -e "$logfile" ] || sudo touch "$logfile"
 
 trap 'exit 1' SIGHUP SIGINT SIGTERM
 
-( sudo bash "$(command -v rsync_tmbackup.sh)" \
-        / "$BACK_LOC" /home/jpartain89/exclude-file.txt & ) &
+{ echo "";
+echo "";
+echo "Started at $(date '+%m-%d-%Y %H:%M:%S')";
+echo "";
+echo ""; } | sudo tee -a "$logfile"
+
+sudo bash -ex "$(command -v rsync_tmbackup.sh)" \
+        / "$BACK_LOC" /home/jpartain89/exclude-file.txt 2>&1 | sudo tee -a "$logfile" && \
+
+{ echo "";
+echo "";
+echo "Ended at $(date '+%m-%d-%Y %H:%M:%S')";
+echo "";
+echo ""; } | sudo tee -a "$logfile"
