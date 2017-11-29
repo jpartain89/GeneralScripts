cbbbff96f764e5826957baf4144716d3a129a81b
jpartain89 <jpartain89@gmail.com>
2017-11-29 07:13:10 -0600

    Updates

--- bash/rsync_crontab.sh
+++ bash/rsync_crontab.sh
@@ -5,6 +5,7 @@
 REMOTE_ADDRESS="jpartain89@192.168.1.15"
 REMOTE_DIR="/Volumes/8TB_EXT/Backups/rpi35/"
 BACK_LOC="${REMOTE_ADDRESS}:${REMOTE_DIR}"
+TMBACKUP="$(command -v rsync_tmbackup.sh)"
 
 logfile="/var/log/$(basename "$0").log"
 [ -e "$logfile" ] || sudo touch "$logfile"
@@ -12,13 +13,12 @@ logfile="/var/log/$(basename "$0").log"
 trap 'exit 1' SIGHUP SIGINT SIGTERM
 
 { echo "";
-echo "";
-echo "Started at $(date '+%m-%d-%Y %H:%M:%S')";
-echo "";
-echo ""; } | sudo tee -a "$logfile"
+  echo "";
+  echo "Started at $(date '+%m-%d-%Y %H:%M:%S')";
+  echo "";
+  echo ""; } | sudo tee -a "$logfile"
 
-sudo bash -ex "$(command -v rsync_tmbackup.sh)" \
-        / "$BACK_LOC" /home/jpartain89/exclude-file.txt 2>&1 | sudo tee -a "$logfile" && \
+sudo bash -ex "$TMBACKUP" / "$BACK_LOC" /home/jpartain89/exclude-file.txt 2>&1 | sudo tee -a "$logfile" && \
 
 { echo "";
 echo "";
