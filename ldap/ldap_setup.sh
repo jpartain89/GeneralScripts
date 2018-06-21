d326d7fed0714312f7ac4fc54115ef14bb6cd063
Justin Partain <jpartain89@gmail.com>
2018-06-20 23:09:32 -0500

    Updates

--- ldap/ldap_setup.sh
+++ ldap/ldap_setup.sh
@@ -3,18 +3,15 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 LDAP_PASS="lover0778"
-LDAP_CMD="ldapadd -x -W -D cn=admin,"
+LDAP_CMD="ldapadd -x -D cn=admin,"
 LDAP_ADDRESS="dc=jpcdi,dc=com -w ${LDAP_PASS}"
 GIT_HOME="/home/jpartain89/git"
 
 FILES=(
-    "${DIR}/newuser.ldif"
-    "${DIR}/disable_anon_bind.ldif"
+    "./newuser .ldif"
+    "./disable_anon_bind.ldif"
 )
 
 for i in "${FILES[@]}"; do
     "${LDAP_CMD}${LDAP_ADDRESS} -f ${i}"
 done
-
-git clone https://github.com/leenooks/phpLDAPadmin.git "${GIT_HOME}/phpldapadmin"
-cp "${GIT_HOME}phpldapadmin/config/config.php{.example,}"
