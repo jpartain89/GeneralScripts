8ed64d0bd7b7d94113a9d4aa914d686f177eb2fa
Justin Partain <jpartain89@gmail.com>
2018-06-20 23:01:49 -0500

    Adding ldap config files

---
+++ ldap/ldap_setup.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+LDAP_PASS="lover0778"
+LDAP_CMD="ldapadd -x -W -D cn=admin,"
+LDAP_ADDRESS="dc=jpcdi,dc=com -w ${LDAP_PASS}"
+GIT_HOME="/home/jpartain89/git"
+
+FILES=(
+    "${DIR}/add_content.ldif"
+    "${DIR}/disable_anon_bind.ldif"
+)
+
+for i in "${FILES[@]}"; do
+    "${LDAP_CMD} ${LDAP_ADDRESS} -f ${i}"
+done
+
+git clone https://github.com/leenooks/phpLDAPadmin.git "${GIT_HOME}/phpldapadmin"
+cp "${GIT_HOME}phpldapadmin/config/config.php{.example,}"
