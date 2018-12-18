1582c5f7fc28a2e5d3a83c6fadeda9325240f2dc
Justin P <jpartain89@gmail.com>
2018-12-18 05:19:41 -0600

    UPdates

--- bash/blocklist_down.sh
+++ bash/blocklist_down.sh
@@ -2,10 +2,14 @@
 
 BLOCKLIST=/usr/local/var/transmission/blocklists/jp_full.txt
 
-# Link good as of June 2018
-# Download lists, unpack and filter, write to stdout
-curl -s https://www.iblocklist.com/lists.php \
-    | sed -n "s/.*value='\(http:.*\?list=.*\)'.*/\1/p" \
-    | xargs wget -O - \
-    | gunzip \
-    | grep -ev '^#' > "$BLOCKLIST" 2>/dev/null &
+main() {
+    # Link good as of June 2018
+    # Download lists, unpack and filter, write to stdout
+    curl -s https://www.iblocklist.com/lists.php \
+        | sed -n "s/.*value='\(http:.*\?list=.*\)'.*/\1/p" \
+        | xargs wget -O - \
+        | gunzip \
+        | egrep -v '^#'
+}
+
+main > "$BLOCKLIST" 2>/dev/null &
