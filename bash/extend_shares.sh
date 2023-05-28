584ad45f01216d70b533285af60e9b2fb7f8130b
Justin P <jpartain89@gmail.com>
2023-05-28 16:20:34 -0500

    Update for 05-28-2023 16-20-34

---
+++ bash/extend_shares.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -e
+
+SHARED_DIRECTORIES=(
+  General
+  CentralShare
+  TV
+  Movies
+  Music
+  Downloads
+  Porn
+)
+
+SYNOSHARE=$(command -v synoshare)
+
+GROUPS=(
+  "Domain Admins"
+  "Domain Users"
+  "Schema Admins"
+  "Enterprise Admins"
+  "DNS Admins"
+  "Group Policy Creator Owners"
+)
+
+GROUPS="$(IFS=,; echo "${GROUPS[*]}")"
+
+for i in "${SHARE_DIRECTORIES[@]}"; do
+  sudo "${SYNOSHARE}" --setuser "/volume2/${i}" RW + "${GROUPS}"
+done
