b331c4e2ea14b3ab1ade7c3ed6e2c4ed742dfb1d
Justin P <jpartain89@gmail.com>
2023-05-28 16:36:50 -0500

    Update for 05-28-2023 16-36-50

--- bash/extend_shares.sh
+++ bash/extend_shares.sh
@@ -22,7 +22,7 @@ GROUPS=(
   "@Group Policy Creator Owners"
 )
 
-GROUPS=$(IFS=",@"  ; echo "${GROUPS[*]}")
+GROUPS=$(IFS=","  ; echo "${GROUPS[*]}")
 
 for i in "${SHARED_DIRECTORIES[*]}"; do
   sudo "${SYNOSHARE}" --setuser "/volume2/${i}" RW + "${GROUPS}"
