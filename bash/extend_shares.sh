361a4101620b0a74e40e3194f27abf6e98eb9124
Justin P <jpartain89@gmail.com>
2023-05-28 16:42:53 -0500

    Update for 05-28-2023 16-42-53

--- bash/extend_shares.sh
+++ bash/extend_shares.sh
@@ -14,16 +14,20 @@ SHARED_DIRECTORIES=(
 SYNOSHARE=$(command -v synoshare)
 
 GROUPS=(
-  "@Domain Admins"
-  "@Domain Users"
-  "@Schema Admins"
-  "@Enterprise Admins"
-  "@DNS Admins"
-  "@Group Policy Creator Owners"
+  @Domain Admins
+  @Domain Users
+  @Schema Admins
+  @Enterprise Admins
+  @DNS Admins
+  @Group Policy Creator Owners
 )
 
-GROUPS=$(IFS=","  ; echo "${GROUPS[*]}")
+for g in "${GROUPS[@]}"; do
+  GROUPS="${g},${groups}"
+done
 
-for i in "${SHARED_DIRECTORIES[*]}"; do
+GROUPS="${GROUPS%,}"
+
+for i in "${SHARED_DIRECTORIES[@]}"; do
   sudo "${SYNOSHARE}" --setuser "/volume2/${i}" RW + "${GROUPS}"
 done
